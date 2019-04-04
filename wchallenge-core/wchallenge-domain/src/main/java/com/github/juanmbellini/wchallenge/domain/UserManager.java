4aa1a8e1aad39c8e7e0b4b0655f61e244ce8524c
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-04-03 21:55:34 -0300

    Define service operations

--- wchallenge-core/wchallenge-domain/src/main/java/com/github/juanmbellini/wchallenge/domain/UserManager.java
+++ wchallenge-core/wchallenge-domain/src/main/java/com/github/juanmbellini/wchallenge/domain/UserManager.java
@@ -16,11 +16,16 @@
 
 package com.github.juanmbellini.wchallenge.domain;
 
+import com.bellotapps.webapps_commons.exceptions.NotImplementedException;
+import com.github.juanmbellini.wchallenge.models.Album;
+import com.github.juanmbellini.wchallenge.models.Photo;
 import com.github.juanmbellini.wchallenge.models.User;
 import com.github.juanmbellini.wchallenge.repositories.UserRepository;
 import com.github.juanmbellini.wchallenge.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Manager for {@link User}s.
@@ -41,5 +46,31 @@ public class UserManager implements UserService {
     @Autowired
     public UserManager(final UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public List<User> getUsers() {
+        throw new NotImplementedException("Not implemented yet");
+    }
+
+    @Override
+    public List<Photo> getAllPhotos() {
+        throw new NotImplementedException("Not implemented yet");
+    }
+
+    @Override
+    public List<Album> getAllAlbums() {
+        throw new NotImplementedException("Not implemented yet");
+    }
+
+    @Override
+    public List<Album> getUserAlbums(final long userId) {
+        throw new NotImplementedException("Not implemented yet");
+    }
+
+    @Override
+    public List<Photo> getUserPhotos(final long userId) {
+        throw new NotImplementedException("Not implemented yet");
     }
 }
--- wchallenge-core/wchallenge-ports/wchallenge-services/src/main/java/com/github/juanmbellini/wchallenge/services/UserService.java
+++ wchallenge-core/wchallenge-ports/wchallenge-services/src/main/java/com/github/juanmbellini/wchallenge/services/UserService.java
@@ -16,10 +16,51 @@
 
 package com.github.juanmbellini.wchallenge.services;
 
+import com.github.juanmbellini.wchallenge.models.Album;
+import com.github.juanmbellini.wchallenge.models.Photo;
 import com.github.juanmbellini.wchallenge.models.User;
+
+import java.util.List;
 
 /**
  * A port into the application that allows {@link User} management.
  */
 public interface UserService {
+
+    /**
+     * Retrieves all the {@link User}s.
+     *
+     * @return The {@link List} of {@link User}s.
+     */
+    List<User> getUsers();
+
+    /**
+     * Retrieves all the {@link Photo}s.
+     *
+     * @return The {@link List} of {@link Photo}s.
+     */
+    List<Photo> getAllPhotos();
+
+    /**
+     * Retrieves all the {@link Album}s.
+     *
+     * @return The {@link List} of {@link Album}s.
+     */
+    List<Album> getAllAlbums();
+
+    /**
+     * Retrieves all the {@link Album}s belonging to a given {@link User}.
+     *
+     * @param userId The {@link User}'s id.
+     * @return The {@link List} of {@link Album}s belonging to the {@link User} with the given {@code userId}.
+     */
+    List<Album> getUserAlbums(final long userId);
+
+    /**
+     * Retrieves all the {@link Photo}s belonging to a given {@link User}.
+     *
+     * @param userId The {@link User}'s id.
+     * @return The {@link List} of {@link Photo}s belonging to the {@link User} with the given {@code userId}.
+     */
+    List<Photo> getUserPhotos(final long userId);
 }
